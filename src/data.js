794cdf0e02e435939bac5824acf451812c4cb3ac
Yogeshdusane9597 <109681871+Yogeshdusane9597@users.noreply.github.com>
2022-10-13 20:59:45 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,114 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Apple iPhone 13 Pro Max",
+    img: "/img/iphone13promax.webp",
+    price: 109990 ,
+    company: "APPLE",
+    info:
+      "Apple iPhone 13 Pro Max is worth all hype with its robust spec sheet. The brand is offering triple cameras on the backside, equipped with multifaceted features and a 12MP selfie lens. This IP68 water-resistant smartphone gets a standard battery with Fast Charging and Wireless Charging support. However, it does not have a fingerprint sensor and FM radio. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung Galaxy S22 Ultra 5G",
+    img: "img/samsung-galaxy-s22-ultra-5g.jpg",
+    price: 108999,
+    company: "SAMSUNG",
+    info:
+      " Samsung Galaxy S22 Ultra 5G smartphone price in India is Rs 108,990. Samsung Galaxy S22 Ultra 5G was launched in the country on February 9, 2022 (Official). Thesmartphone comes in 3 other storage and RAM variants - Samsung-Galaxy-S22-Ultra-5G-256-GB-12-GB, Samsung-Galaxy-S22-Ultra-5G-512-GB-12-GB, Samsung-Galaxy-S22-Ultra-5G-1-TB-12-GB. As for the colour options, the Samsung Galaxy S22 Ultra 5G smartphone comes in Phantom Black, Phantom White, Green, Burgundy colours  ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "vivo X80 Pro Plus 5G",
+    img: "img/vivo.webp",
+    price: 79990,
+    company: "VIVO",
+    info:
+      "Vivo X80 Pro Plus 5G carries a 6.78-inch AMOLED display that offers Quad HD+ resolution at 1440x3200 pixels. The screen produces pixel density of 518 PPI, and has punch hole layout at the top-centre. The display offers large viewing area that gives you screen to body ratio of 86 percent. The power button and the volume rocker are on the right side of the phone. For security purpose, this phone has an on-screen fingerprint sensor built under the display and face unlock that works through the selfie camera.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Samsung Galaxy Z Fold4 5G ",
+    img: "img/zfold.jfif",
+    price: 154999,
+    company: "SAMSUNG",
+    info:
+      "Dynamic AMOLED ,displayPowerful camera setup on both end , sQualcomm Snapdragon 8 Plus Gen 1 chipset , Massive 12GB RAM , 4400mAh battery compatible with 25W Fast Charging , Wireless charging , 256GB internal storage , Foldable design , Sidewise fingerprint sensor",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Xiaomi 12 Pro 5G",
+    img: "img/12-pro-5g-2201122g-xiaomi-original-image2z8ksjygxdk.webp",
+    price: 54999,
+    company: "XIOMI",
+    info:
+      "Xiaomi Mi12 Pro 5G is fitted with a tall 6.7-inch AMOLED touchscreen with a punch-hole. It comes with a 2K resolution display along with 1440x3200px and a pixel density of 522ppi. On top of that, the panel has Dolby Vision and HDR 10+ support for better picture clarity. Also, it is protected with Gorilla Glass Victus for improved durability. The smartphone brings a unique camera bump with a dual-LED dual-tone flash. It has a 50MP primary camera with dual PDAF and OIS support, another 50MP ultrawide camera, and a 50MP telephoto sensor with 2x optical zoom. Also, the rear camera is capable of recording UHD 8K videos at @24 fps. Besides, it has a 32MP selfie camera under the punch-hole notch.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "OnePlus 10T 5G",
+    img: "img/oneplus-10T-5G.png",
+    price: 61999,
+    company: "One Plus",
+    info:
+      "OnePlus 10 Pro is yet another premium flagship offering from the brand. The smartphone promises seamless visuals and detailed portraits. It continues the OnePlus legacy with a powerful processor configuration backed by a massive 8GB RAM. Moreover, a 5000mAh battery with 80W Super VOOC Charging capability ensures prolonged usage..",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "iQOO 9 Pro 5G",
+    img: "img/iq9pro.webp",
+    price: 57990 ,
+    company: "IQ",
+    info:
+      "Wide 2K E5 AMOLED display , Intelligent Display Chip , Impressive triple camera setup on the rear side , 16MP Front Camera with Screen Flash , Qualcomm Snapdragon 8 Gen 1 chipset , 8GB LPDDR5 RAM , Fast and Wireless Charging compatible battery , 256GB internal storage , 3D Ultrasonic Fingerprint Sensor . ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "realme GT Neo 3 5G 150W",
+    img: "img/realme-gt-neo-3-5g.jpg",
+    price: 39999 ,
+    company: "realme",
+    info:
+      "Corning Gorilla Glass protective display, Three lenses on the rear side, 16MP Selfie Shooter at the front, MediaTek Dimensity 8100 chipset, 12GB LPDDR5 RAM, 150W Super Dart Charging, 256GB internal storage, On-screen Optical fingerprint sensor, Heart Rate Monitor, Bluetooth v5.3, X-axis Linear Motor.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+
+  
+];
+
+export const detailProduct = {
+  id: 1,
+    title: "Apple iPhone 13 Pro Max",
+    img: "/img/iphone13promax.webp",
+    price: 109990 ,
+    company: "APPLE",
+    info:
+      "Apple iPhone 13 Pro Max is worth all hype with its robust spec sheet. The brand is offering triple cameras on the backside, equipped with multifaceted features and a 12MP selfie lens. This IP68 water-resistant smartphone gets a standard battery with Fast Charging and Wireless Charging support. However, it does not have a fingerprint sensor and FM radio. ",
+    inCart: false,
+    count: 0,
+    total: 0
+};
